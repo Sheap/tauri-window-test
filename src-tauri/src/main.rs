b22a367e8c6725faf4603ffaec72618d43437e85
Stuart Heap <stuart.heap@motius.de>
2022-06-01 11:28:59 +0200

    first commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,42 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+use tauri::Manager;
+
+#[tauri::command]
+fn toggle_popout(window: tauri::Window) {
+  println!("toggling");
+  if let Some(popout_window) = window.get_window("popout") {
+    println!("found window");
+    if popout_window.is_visible().unwrap() {
+      match popout_window.hide() {
+        Ok(()) => {
+          println!("hidden");
+        },
+        Err(err) => {
+          println!("error hiding: {}", err);
+        }
+      }
+    } else {
+      match popout_window.show() {
+        Ok(()) => {
+          println!("shown");
+        },
+        Err(err) => {
+          println!("error showing: {}", err);
+        }
+      }
+    }
+  }
+}
+
+fn main() {
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![
+      toggle_popout,
+    ])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	async function togglePopOut () {
+		await invoke('toggle_popout');
+	}
+</script>
+
+<main>
+	<h1>Popout Test</h1>
+	<button on:click={togglePopOut}>Toggle Popout</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
